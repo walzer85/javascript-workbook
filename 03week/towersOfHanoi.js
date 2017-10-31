5ff6866abef67d9047f07de5775b8cb08b58d9d8
walzer85 <walzer85@gmail.com>
2017-10-30 21:16:38 -0500

    adding function to reset the board

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -7,40 +7,53 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//This is the initial setting at the beginning of towersOfHanoi.  Part of premade code
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
 
-function printStacks() {
+//This function prints the stacks on to the screen.  Part of premade code
+let printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece(startstack, endStack) {
+//printBeginningStacks, reset the var stacks to its original position, nest stacks inside
+const printBeginningStacks=(stacks)=> {}
+  let stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+  };
+//movePiece(), Moves the last index of the startStack to the last index of the endStack, lastindexof method
+const movePiece=(startstack, endStack)=> {
     endStack.push(startstack.lastindexof)
     startstack.lastindexof.slice
   }
 
+//isLegal(), checks to see if the last index of the start stack is less than the end stack last index.  If the value is less, run movePiece.  If the value is greater, deny the move, lastindexof method
 const isLegal=(startstack, endStack)=> {
-  if(startstack.lastindexof > endStack.lastindexof) {
+  if((startstack.lastindexof < endStack.lastindexof) || (startstack.lastindexof > 0)) {
     movePiece
   } else {
     return 'Not a legal move.';
   }
 }
 
-function checkForWin() {
+//checkForWin,
+const checkForWin=()=>
   if (stacks === [a: [], b: [1], c:[4, 3, 2, 1]]) {
     return "You win!  Let's play again!";
+
   } else
 
 }
 
 function towersOfHanoi(startStack, endStack) {
-  moviePiece
+  isLegal
 }
 
 function getPrompt() {
