e1565ff2697de8614a153452c98eca5a0c3b6ce4
walzer85 <walzer85@gmail.com>
2017-11-28 20:20:34 -0600

    figuring out what I'd like to do

---
+++ 08week/personalreactapp/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppBar from 'material-ui/AppBar';
+
+
+class Personal extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div className="App">
+      const AppBarExampleIcon = () => (
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+);
+      </div>
+    );
+  }
+}
+
+export default Personal;
