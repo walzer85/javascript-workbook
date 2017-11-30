d6d3d1119f50c18561593144b04f435c0b4c71d5
walzer85 <walzer85@gmail.com>
2017-11-29 20:45:46 -0600

    fixing the main app.js to reflect the branches

---
+++ 08week/personalreactapp/src/home.js
@@ -0,0 +1,74 @@
+import React { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
+class OurBand extends Component {
+  constructor(props) {
+    super(props);
+
+      this.state = {
+      name: '',
+      instrument: '',
+      yearsPlayed: '',
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.name,
+      instrument : e.target.instrument,
+      yearsPlayed: e.target.yearsPlayed
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <MuiThemeProvider>
+        <AppBar
+          title="Let's Make a Band!!"
+          iconClassNameRight="muidocs-icon-navigation-expand-more">
+          <IconMenu
+             iconButtonElement={
+               <IconButton><MoreVertIcon /></IconButton>
+             }
+             targetOrigin={{horizontal: 'right', vertical: 'top'}}
+             anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+           >
+             <MenuItem primaryText="Our Band" />
+             <MenuItem primaryText="Setups" />
+             <MenuItem primaryText="Hear Some Bands" />
+           </IconMenu>
+         </AppBar>
+        <TextField
+        value={this.state.name}
+        floatingLabelText="What is your name?"
+        onChange={this.handleChange}
+      />
+      <TextField
+          value={this.state.instrument}
+          floatingLabelText="What instrument do you play?"
+          onChange={this.handleChange}
+      />
+      <TextField
+        value={this.state.yearsPlayed}
+        floatingLabelText="How long have you played it?"
+        onChange={this.handleChange}
+      />
+      </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default OurBand;
