a60e9af80e7de3b5ae7b7755a30ce6b99ee1f38e
walzer85 <walzer85@gmail.com>
2017-12-02 14:38:27 -0600

    fixed checkforwin

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -8,14 +8,14 @@ const rl = readline.createInterface({
 });
 
 //This is the initial setting at the beginning of towersOfHanoi.  Part of premade code
-let stacks = {
+let Stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
 
 //This function prints the stacks on to the screen.  Part of premade code
-let printStacks() {
+let printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
@@ -50,11 +50,12 @@ const isLegal = (startStack, endStack) => {
 }
 
 //checkForWin, if all of the numbers are in the last stack, run the win sequence, which should reset the object, if statement
-const checkForWin=()=>
-  if (stacks === [a: [], b: [1], c:[4, 3, 2, 1]]) {
+const checkForWin = () => {
+  if (stacks[c].length === 3) {
     console.log("You win!  Let's play again!");
-    printBeginningStacks;
-    getPrompt;
+  } else {
+    console.log('Keep trying, you\'ll get there!')
+    return false;
   }
 }
 
