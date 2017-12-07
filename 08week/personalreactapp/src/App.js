9a6944124af2f04af0d600a221ee47319e9508fc
walzer85 <walzer85@gmail.com>
2017-12-07 06:56:20 -0600

    fixed existing errors to get to a working state with links, need to start adding components

--- 08week/personalreactapp/src/App.js
+++ 08week/personalreactapp/src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 
-import {BrowserRouter as Router, Link, Match, Miss} from 'react-router';
+import {BrowserRouter as Router, Link, Match, Miss} from 'react-router-dom';
 
 //would like to accept inputs of a name, the instrument they play, and how many years they've played it
 //would like to print out the values a user as entered
 //would like to classify by instrument
 
-const App = () => {
+class App extends React.Component {
   render() {
-  return (
-    <Router>
-    <div>
-      <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/setup">Ensemble Setups</Link></li>
-        <li><Link to="/hearbands">Hear Some Bands</Link></li>
-      </ul>
-    </div>
-  );
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/setup">Ensemble Setups</Link></li>
+            <li><Link to="/hearbands">Hear Some Bands</Link></li>
+          </ul>
+        </div>
+      </Router>
+    );
   }
 }
 
