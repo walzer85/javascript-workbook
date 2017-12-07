fcfa82c3a672cc93a67513fe32df980794f654c0
walzer85 <walzer85@gmail.com>
2017-12-07 07:03:00 -0600

    moving the home page back to app.js and adding separate files from there rather than everything on a separate file

--- 08week/personalreactapp/src/App.js
+++ 08week/personalreactapp/src/App.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import {BrowserRouter as Router, Link, Match, Miss} from 'react-router-dom';
 
 //would like to accept inputs of a name, the instrument they play, and how many years they've played it
@@ -19,18 +29,42 @@ const hearBands = () => {
 };
 
 class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <ul>
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/setup">Ensemble Setups</Link></li>
-            <li><Link to="/hearbands">Hear Some Bands</Link></li>
-          </ul>
-        </div>
-      </Router>
-    );
+  return (
+    <div className="App">
+      <MuiThemeProvider>
+      <AppBar
+        title="Let's Make a Band!!"
+        iconClassNameRight="muidocs-icon-navigation-expand-more">
+        <IconMenu
+           iconButtonElement={
+             <IconButton><MoreVertIcon /></IconButton>
+           }
+           targetOrigin={{horizontal: 'right', vertical: 'top'}}
+           anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+         >
+           <MenuItem primaryText="Our Band" />
+           <MenuItem primaryText="Setups" />
+           <MenuItem primaryText="Hear Some Bands" />
+         </IconMenu>
+       </AppBar>
+      <TextField
+      value={this.state.name}
+      floatingLabelText="What is your name?"
+      onChange={this.handleChange}
+    />
+    <TextField
+        value={this.state.instrument}
+        floatingLabelText="What instrument do you play?"
+        onChange={this.handleChange}
+    />
+    <TextField
+      value={this.state.yearsPlayed}
+      floatingLabelText="How long have you played it?"
+      onChange={this.handleChange}
+    />
+    </MuiThemeProvider>
+    </div>
+  );
   }
 }
 
