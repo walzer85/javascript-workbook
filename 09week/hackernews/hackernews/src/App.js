902f048971987cb591ef3f8473b86a2ee7bda9ac
walzer85 <walzer85@gmail.com>
2017-12-07 06:29:09 -0600

    still not really understanding how to pass stuff between a parent and child component

--- 09week/hackernews/hackernews/src/App.js
+++ 09week/hackernews/hackernews/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +6,26 @@ class App extends Component {
   componentDidMount(){
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((res) => {
       res.json().then((storyIds) => {
-        console.log(storyIds);
+        storyIds.forEach((storyId) => {
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((res) => {
+            res.json().then((story) => {
+              console.log(story);
+            })
+          });
+        });
       });
     });
+}
+
+  stories(){
+    fetch
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hacker News by Me, not really though.</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
