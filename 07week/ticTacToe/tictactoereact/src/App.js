35ea88e4cf52aaca896ea02271219abe44ed6c8e
walzer85 <walzer85@gmail.com>
2017-12-07 20:35:58 -0600

    need to make the board next

--- 07week/ticTacToe/tictactoereact/src/App.js
+++ 07week/ticTacToe/tictactoereact/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 //I need to make the squares for the Xs and Os to be held
+//I need to allow the square to be clicked so you can add the X or O
 const Square = (props) => {
   return (
     <button className="square" onClick={() => props.onClick()}>
@@ -10,3 +11,5 @@ const Square = (props) => {
     </button>
   );
 }
+
+//I need to make a board consisting of the squares I created above
