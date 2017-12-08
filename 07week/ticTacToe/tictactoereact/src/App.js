7bcecb48b75b860d181ca16e4ac74e222af73b0f
walzer85 <walzer85@gmail.com>
2017-12-07 20:30:40 -0600

    made the square a button

--- 07week/ticTacToe/tictactoereact/src/App.js
+++ 07week/ticTacToe/tictactoereact/src/App.js
@@ -1,3 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//I need to make the squares for the Xs and Os to be held
+const Square = (props) => {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+    {props.value}
+    </button>
+  );
+}
