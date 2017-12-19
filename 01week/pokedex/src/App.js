30a7cab24ddcf3cbecf03acf1d1a291d34230d9f
walzer85 <walzer85@gmail.com>
2017-12-19 06:42:52 -0600

    struggling to find an api that I understand and still works

---
+++ 01week/pokedex/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppBar from 'material-ui/AppBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar title='Help!' />
+      </div>
+    )
+  }
+
+
+}
+
+export default App;
