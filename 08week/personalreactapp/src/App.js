72549dbe8f4751144d0e85663c680d08d483d67b
walzer85 <walzer85@gmail.com>
2017-12-26 15:14:09 -0600

    made the three submission inputs, not directing anywhere real yet

--- 08week/personalreactapp/src/App.js
+++ 08week/personalreactapp/src/App.js
@@ -1,104 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import TextField from 'material-ui/TextField';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import {BrowserRouter as Router, Link, Match, Miss} from 'react-router-dom';
 
-//would like to accept inputs of a name, the instrument they play, and how many years they've played it
-//would like to print out the values a user as entered
-//would like to classify by instrument
 
-class OurBand extends Component {
-    constructor(props) {
-      super(props);
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-      this.state = {
-      name: '',
-      instrument: '',
-      yearsPlayed: '',
-    }
-  };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleToggle = (e) => {
-      e.preventDefault();
-      this.setState({open: false});
-    }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
-    handleClose = () => this.setState({open: false});
-
-    handleChange = (e) => {
-      e.preventDefault();
-      this.setState({
-        ...this.state,
-        name: e.target.name,
-        instrument : e.target.instrument,
-        yearsPlayed: e.target.yearsPlayed
-      });
-    };
-
-    // const home = () => {
-    //
-    // };
-    //
-    // const setup = () => {
-    //
-    // };
-    //
-    // const hearBands = () => {
-    //
-    // };
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <MuiThemeProvider>
-        <AppBar
-          title="Let's Make a Band!!"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-        <RaisedButton label='Open Drawer' onClick={this.handleToggle} />
-        <Drawer ref='Drawer' docked='false'
-          home={this.home}
-          instrument={this.instrument}
-          yearsPlayed={this.yearsPlayed}
-          open={this.state.open}
-          onRequestChange={open => this.setState({open})}
-        >
-        <MenuItem onClick={this.handleClose}>Home</MenuItem>
-        <MenuItem onClick={this.handleClose}>Setup</MenuItem>
-        <MenuItem onClick={this.handleClose}>Hear Some Bands!</MenuItem>
-        </Drawer>
-      <TextField
-        id='text-field'
-        value={this.state.name}
-        floatingLabelText="What is your name?"
-        onChange={this.handleChange}
-      />
-      <TextField
-          value={this.state.instrument}
-          floatingLabelText="What instrument do you play?"
-          onChange={this.handleChange}
-      />
-      <TextField
-        id='text-field'
-        value={this.state.yearsPlayed}
-        floatingLabelText="How long have you played it?"
-        onChange={this.handleChange}
-      />
-      </MuiThemeProvider>
+          <AppBar title='Make A Band!' />
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Instrument:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Years Played:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
-export default OurBand;
+export default NameForm;
--- 08week/personalreactapp/src/hearbands.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HearBands = () => {
-    return (
-        <div>
-
-        </div>
-    );
-};
