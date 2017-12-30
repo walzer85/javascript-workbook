ffec27cb57b047c0f698f13b5077bf552761ea9d
walzer85 <walzer85@gmail.com>
2017-12-30 15:09:43 -0600

    got the correct items to display, text fields not accepting input

--- 08week/personalreactapp/src/App.js
+++ 08week/personalreactapp/src/App.js
@@ -18,30 +18,16 @@ class BandForm extends React.Component {
       yearsPlayed: []
     };
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    
   }
 
-  handleChange(event) {
+  handleSubmit(event) {
     this.setState({
       name: event.target.name,
       instrument: event.target.instrument,
       yearsPlayed: event.target.yearsPlayed
     });
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.name);
-    alert('A instrument was submitted: ' + this.state.instrument);
-    alert('Years played was submitted: ' + this.state.yearsPlayed);
-    event.preventDefault();
-    // render() {
-    //   return(
-    //     <div>
-    //       //this is where the card with the inputs displayed will go
-    //     </div>
-    //   )
-    // }
   }
 
   render() {
@@ -52,17 +38,17 @@ class BandForm extends React.Component {
           <form className='FormStyling' onSubmit={this.handleSubmit}><br />
             <label>
               Name:
-              <input type="text" value={this.state.name} onChange={this.handleChange(this, 'name')} />
+              <input type="text" value={this.state.name} />
             </label><br />
             <input type="submit" value="Submit" /><br />
             <label>
               Instrument:
-              <input type="text" value={this.state.instrument} onChange={this.handleChange(this, 'instrument')} />
+              <input type="text" value={this.state.instrument} />
             </label><br />
             <input type="submit" value="Submit" /><br />
             <label>
               Years Played:
-              <input type="text" value={this.state.yearsPlayed} onChange={this.handleChange(this, 'yearsPlayed')} />
+              <input type="text" value={this.state.yearsPlayed} />
             </label><br />
             <input type="submit" value="Submit" />
           </form>
