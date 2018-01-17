767a1b853bbeb103b4bbaa081ee0bb8158d4e129
walzer85 <walzer85@gmail.com>
2018-01-16 20:06:28 -0600

    adding the results box

--- 08week/personalreactapp/src/playerdisplay.js
+++ 08week/personalreactapp/src/playerdisplay.js
@@ -2,6 +2,12 @@ import { Component } from 'react'
 
 class PlayerDisplay extends Component {
   render () {
-    return ()
+    return (
+      <div className='results'>
+        <h2>Band Mate</h2><br>
+        <div className='newplayer'>
+          
+        </div>
+    )
   }
 }
